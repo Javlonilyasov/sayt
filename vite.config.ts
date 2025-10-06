039ee3642f7aab32d1d22ef6fb6a91d3c0a41567
Javlonilyasov <167566038+Javlonilyasov@users.noreply.github.com>
2025-10-06 20:52:46 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,14 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
   base: './',
   build: {
-    outDir: 'dist', // ✅ 'build' emas
-    emptyOutDir: true, // eski build fayllarni tozalaydi
+    outDir: 'dist',
+    emptyOutDir: true,
     target: 'esnext',
   },
   server: {
